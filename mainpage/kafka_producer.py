17c62eb45e84980f465bad7b8a17823fd948000d
wh123rus <wh123rus@naver.com>
2023-11-09 07:16:43 +0000

    Chage kafka_producer security_protocol SASL

--- mainpage/kafka_producer.py
+++ mainpage/kafka_producer.py
@@ -3,10 +3,21 @@ import os, traceback
 
 def send_kafka_message(message, topic='quickstart-events'):
     broker = os.getenv('BROKER_ENV', 'localhost:9092')
+    sasl_user = os.getenv('SASL_USER', 'user')
+    sasl_passwd = os.getenv('SASL_PASSWORD', 'user')
+
     try:
         data = (message).encode('utf-8')
+
+        broker_params= {
+            "bootstrap_servers" : broker,
+            "compression_type" : 'snappy',
+            "security_protocol" : 'SASL_PLAINTEXT',
+            "sasl_plain_username" : sasl_user,
+            "sasl_plain_password" : sasl_passwd
+        }
         
-        producer = KafkaProducer(bootstrap_servers=broker, compression_type='snappy')
+        producer = KafkaProducer(**broker_params)
         if producer.bootstrap_connected():
             producer.send(topic, value=data)
             producer.flush()
